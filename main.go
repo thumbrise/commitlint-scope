8165f35d6ac97f0c90ce63f2e0b8fd51184797fd
Ruslan Kokoev <ruslan.kokoev.1999@gmail.com>
2026-05-29 02:38:27 +0300

    feat: Add version, commit, date ldflags

--- main.go
+++ main.go
@@ -8,10 +8,21 @@ import (
 	"github.com/thumbrise/commitlint-scope/cmd"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
 	ctx := context.Background()
 
-	if err := cmd.Root.Run(ctx, os.Args); err != nil {
+	root := cmd.NewRootCMD(cmd.RootCMDLDFlags{
+		Version: version,
+		Commit:  commit,
+		Date:    date,
+	})
+	if err := root.Run(ctx, os.Args); err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "fatal error: %[1]v\n", err)
 
 		os.Exit(1)
