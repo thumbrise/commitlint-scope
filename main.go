e7d0903614f97f573634b34597d57aefe57ba111
Ruslan Kokoev <ruslan.kokoev.1999@gmail.com>
2026-05-28 08:36:57 +0300

    feat: Add JSON, color, verbose output modes, add text output for default UX, configure logger AddSource dynamically

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"log/slog"
 	"os"
 
 	"github.com/thumbrise/commitlint-scope/cmd"
@@ -12,22 +11,9 @@ import (
 func main() {
 	ctx := context.Background()
 
-	configureLogger()
-
 	if err := cmd.Root.Run(ctx, os.Args); err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "error: %[1]v\n", err)
 
 		os.Exit(1)
 	}
-}
-
-func configureLogger() {
-	handler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
-		AddSource: true,
-		Level:     nil,
-	})
-
-	logger := slog.New(handler)
-
-	slog.SetDefault(logger)
 }
