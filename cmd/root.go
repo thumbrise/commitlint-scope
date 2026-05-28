7b0f45b1d1d7fef345bad15da8fbc4a41aa0149c
Ruslan Kokoev <ruslan.kokoev.1999@gmail.com>
2026-05-29 02:38:27 +0300

    feat: Add version, commit, date ldflags

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"context"
 	"errors"
+	"fmt"
 	"os"
 
 	"github.com/fatih/color"
@@ -10,25 +11,43 @@ import (
 	"github.com/urfave/cli/v3"
 )
 
-// Root is the entry point command for commitlint-scope.
-var Root = &cli.Command{
-	Name:        "commitlint-scope",
-	Description: `commitlint-scope - a linter that checks if declared commit scopes match the changed files`,
-	Commands: []*cli.Command{
-		commands.RunCMD,
-		commands.InitCMD,
-	},
-	Suggest: true,
-	ExitErrHandler: func(ctx context.Context, command *cli.Command, err error) {
-		code := 1
+type RootCMDLDFlags struct {
+	Version string
+	Commit  string
+	Date    string
+}
 
-		if coder, ok := errors.AsType[cli.ExitCoder](err); ok {
-			code = coder.ExitCode()
-		}
+type RootCMD struct {
+	cmd *cli.Command
+}
 
-		_, _ = color.New(color.FgRed, color.Bold).Fprintf(os.Stderr, "\n%s\n", err)
-		_, _ = color.New(color.FgRed, color.Bold).Fprintf(os.Stderr, "\nexit code %d\n", code)
+func NewRootCMD(ldflags RootCMDLDFlags) *RootCMD {
+	return &RootCMD{
+		cmd: &cli.Command{
+			Name:    "commitlint-scope",
+			Usage:   "linter that checks if declared commit scopes match the changed files",
+			Version: fmt.Sprintf("v%s %s %s", ldflags.Version, ldflags.Commit, ldflags.Date),
+			Commands: []*cli.Command{
+				commands.RunCMD,
+				commands.InitCMD,
+			},
+			Suggest: true,
+			ExitErrHandler: func(ctx context.Context, command *cli.Command, err error) {
+				code := 1
 
-		os.Exit(code)
-	},
+				if coder, ok := errors.AsType[cli.ExitCoder](err); ok {
+					code = coder.ExitCode()
+				}
+
+				_, _ = color.New(color.FgRed, color.Bold).Fprintf(os.Stderr, "\n%s\n", err)
+				_, _ = color.New(color.FgRed, color.Bold).Fprintf(os.Stderr, "\nexit code %d\n", code)
+
+				os.Exit(code)
+			},
+		},
+	}
+}
+
+func (c *RootCMD) Run(ctx context.Context, args []string) error {
+	return c.cmd.Run(ctx, args)
 }
